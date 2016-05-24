d0581466745caf784d4d4ff2b4024f0dfdc762b4
sarrufat <sarrufat.feina@gmail.com>
2016-05-24 13:02:18 +0200

    File Cleaner II

--- src/main/scala/curam/util/oracle/cleaner/LineCleaner.scala
+++ src/main/scala/curam/util/oracle/cleaner/LineCleaner.scala
@@ -1,19 +1,23 @@
 package curam.util.oracle.cleaner
 
 import scala.io.Source
+import java.io.PrintWriter
+import java.io.File
 
 // Skip INSERT LINES UNTIL ');'
 object LineCleaner extends App {
 
-  def cleanFile(source: String) = {
+  def cleanFile(source: String, output: String) = {
     val ss = Source.fromFile(source)
+    val pw = new PrintWriter(new File(output))
     for (line ← ss.getLines()) {
       if (!(line.startsWith("INSERT INTO") || line.startsWith("insert into"))) {
-        println(line);
+        pw.println(line)
       }
     }
+    pw.close()
   }
-  assert(args.size == 1)
-  cleanFile(args(0))
+  assert(args.size == 2)
+  cleanFile(args(0), args(1))
   // cleanFile("./src/test/resources/SetupDatabaseCleanTest.sql")
 }
